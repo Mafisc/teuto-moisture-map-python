840e06b82ef84bd6fbf594af5ad4739311549195
matthias <mafisc@gmail.com>
2022-02-23 21:06:06 +0100

    WIP: First working extraction and export to json for map data

--- tmm/scripts/export_influx.py
+++ tmm/scripts/export_influx.py
@@ -1,34 +1,37 @@
+from datetime import datetime
+import json
 from influxdb_client import InfluxDBClient
 import sys
+import calendar
 
 def export_to_json(path_to_config="../config.ini", query_type='value'):
 
     value_query = 'from(bucket: "tmm-bucket") \
-    |> range(start: v.timeRangeStart, stop:v.timeRangeStop) \
+    |> range(start: -24h) \
     |> filter(fn: (r) => \
         r._measurement == "moisture" and \
         r._field == "percent"'
 
     map_query = 'lat = from(bucket: "tmm-bucket") \
-    |> range(start: v.timeRangeStart, stop: v.timeRangeStop) \
+    |> range(start: -24h) \
     |> filter(fn: (r) => r["_measurement"] == "moisture") \
     |> filter(fn: (r) => r["_field"] == "latitude") \
     |> aggregateWindow(every: 1d , fn: last) \
     \
     long = from(bucket: "tmm-bucket") \
-    |> range(start: v.timeRangeStart, stop: v.timeRangeStop) \
+    |> range(start: -24h) \
     |> filter(fn: (r) => r["_measurement"] == "moisture") \
     |> filter(fn: (r) => r["_field"] == "longitude") \
     |> aggregateWindow(every: 1d , fn: last) \
     \
     moisture = from(bucket: "tmm-bucket") \
-    |> range(start: v.timeRangeStart, stop: v.timeRangeStop) \
+    |> range(start: -24h) \
     |> filter(fn: (r) => r["_measurement"] == "moisture") \
     |> filter(fn: (r) => r["_field"] == "percent") \
     |> aggregateWindow(every: 1d , fn: last) \
     \
     alt = from(bucket: "tmm-bucket") \
-    |> range(start: v.timeRangeStart, stop: v.timeRangeStop) \
+    |> range(start: -24h) \
     |> filter(fn: (r) => r["_measurement"] == "moisture") \
     |> filter(fn: (r) => r["_field"] == "altitude") \
     |> aggregateWindow(every: 1d , fn: last) \
@@ -48,6 +51,25 @@ def export_to_json(path_to_config="../config.ini", query_type='value'):
         query_api = client.query_api()
         result = query_api.query(query=query)
         print(result)
+
+        valueArray = []
+        for record in result[0].records:
+            jsonRecord = {}
+            jsonRecord['device'] = record.values['device']
+            jsonRecord['altitude'] = record.values['altitude']
+            jsonRecord['percent'] = record.values['percent']
+            jsonRecord['latitude'] = record.values['latitude']
+            jsonRecord['longitude'] = record.values['longitude']
+            valueArray.append(jsonRecord)
+        
+        jsonObj = {}
+        jsonObj['timestamp'] = str(datetime.now())
+        jsonObj['records'] = valueArray
+
+        retval = json.dumps(jsonObj)
+        print(retval)
+
+
 
 args = sys.argv[1:]
 path = args[0]
